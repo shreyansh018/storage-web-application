2849d764665d9ff516e7d14763f774f8192fb1c8
shreyansh018 <shreyansh.singhrajput2019@gmail.com>
2025-01-01 23:17:16 +0530

    es lint disable

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -58,6 +58,7 @@ const FileUploader = ({ ownerId, accountId, className }: Props) => {
 
       await Promise.all(uploadPromises);
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [ownerId, accountId, path]
   );
 
